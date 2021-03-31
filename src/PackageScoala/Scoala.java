e1ec3e0373ef4a7bb55188e38277b3e901760620
dandi08 <dandi0801@gmail.com>
2021-03-31 20:52:51 +0300

    Etapa 1 - Proiect Java

---
+++ src/PackageScoala/Scoala.java
@@ -0,0 +1,47 @@
+package PackageScoala;
+import java.util.*;
+import PackageClasa.*;
+import PackageMaterie.Materie;
+
+public class Scoala {
+    private String nume;
+    private String adresa;
+    private HashMap<String, Clasa> clase = new HashMap<>();
+
+    public Scoala() {
+    }
+
+    public Scoala(String nume, String adresa, HashMap<String, Clasa> clase) {
+        this.nume = nume;
+        this.adresa = adresa;
+        this.clase = clase;
+    }
+
+    public void adaugaClasa(Clasa clasa){
+        clase.put(clasa.getNumeClasa(), clasa);
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
+    }
+
+    public HashMap<String, Clasa> getClase() {
+        return clase;
+    }
+
+    public void setClase(HashMap<String, Clasa> clase) {
+        this.clase = clase;
+    }
+}
